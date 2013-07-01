073caf5d99d18dae42b037a7459f4201243b6438
Andreas Böhrnsen <andreas@boehrnsen.de>
2013-07-01 19:59:59 +0800

    initial commit

---
+++ lib/vagrant-package_cache.rb
@@ -0,0 +1,20 @@
+require "pathname"
+
+require "vagrant-package_cache/plugin"
+
+module VagrantPlugins
+  module PackageCache
+    lib_path = Pathname.new(File.expand_path("../vagrant-package_cache", __FILE__))
+    autoload :Action, lib_path.join("action")
+    autoload :Errors, lib_path.join("errors")
+
+    # This returns the path to the source of this plugin.
+    #
+    # @return [Pathname]
+    def self.source_root
+      @source_root ||= Pathname.new(File.expand_path("../../", __FILE__))
+    end
+  end
+end
+
+# vim:set ts=2 et:
---
+++ lib/vagrant-package_cache/version.rb
@@ -0,0 +1,5 @@
+module Vagrant
+  module PackageCache
+    VERSION = "0.0.1"
+  end
+end
