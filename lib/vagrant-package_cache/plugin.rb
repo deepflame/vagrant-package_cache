221f28064629b3322535c52bccd45b95752f803b
Andreas Böhrnsen <andreas@boehrnsen.de>
2013-07-03 15:16:59 +0800

    remove template code

--- lib/vagrant-package_cache/plugin.rb
+++ lib/vagrant-package_cache/plugin.rb
@@ -18,49 +18,9 @@ module VagrantPlugins
         This plugin caches downloaded Linux packages in the host machine, thus making testing of provisionings with Chef, Puppet, etc. faster.
       DESC
 
-      config(:aws, :provider) do
-        require_relative "config"
-        Config
+
       end
 
-      provider(:aws, parallel: true) do
-        # Setup logging and i18n
-        setup_logging
-        setup_i18n
-
-        # Return the provider
-        require_relative "provider"
-        Provider
-      end
-
-      # This sets up our log level to be whatever VAGRANT_LOG is.
-      def self.setup_logging
-        require "log4r"
-
-        level = nil
-        begin
-          level = Log4r.const_get(ENV["VAGRANT_LOG"].upcase)
-        rescue NameError
-          # This means that the logging constant wasn't found,
-          # which is fine. We just keep `level` as `nil`. But
-          # we tell the user.
-          level = nil
-        end
-
-        # Some constants, such as "true" resolve to booleans, so the
-        # above error checking doesn't catch it. This will check to make
-        # sure that the log level is an integer, as Log4r requires.
-        level = nil if !level.is_a?(Integer)
-
-        # Set the logging level on all "vagrant" namespaced
-        # logs as long as we have a valid level.
-        if level
-          logger = Log4r::Logger.new("vagrant-package_cache")
-          logger.outputters = Log4r::Outputter.stderr
-          logger.level = level
-          logger = nil
-        end
-      end
     end
   end
 end
